4d647644ccd897b654b62c80c1521f829e07037b
Devin McGloin <devin@devinmcgloin.com>
2016-05-31 17:54:09 -0400

    updated version number

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 setup(
     name='python-smooch',
     packages=find_packages(exclude=['docs', 'tests']),
-    version='v0.2.1',
+    version='v1.0.0',
     description='A Python wrapper for the Smooch API',
     license='MIT',
     author='Devin McGloin',
     author_email='devin@devinmcgloin.com',
     url='https://github.com/devinmcgloin/python-smooch',
-    download_url='https://github.com/devinmcgloin/python-smooch/tarball/v0.2.1',
+    download_url='https://github.com/devinmcgloin/python-smooch/tarball/v1.0.0',
     keywords=['messaging', 'smooch'],  # arbitrary keywords
     install_requires=[
         'PyPWT',
